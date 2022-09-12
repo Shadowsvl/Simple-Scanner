6a45cbb5eb38bed7e348ea654c3ff2492b268a4d
Sergio Vázquez <svl_shadow@outlook.com>
2022-09-12 18:22:38 -0500

    Simple Scanner project created. Main dependencies, configuration, theme and assets done.

---
+++ app/src/main/java/com/heka/simplescanner/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.heka.simplescanner.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightBackground = Color(0xFFF3F3F3)
+val DarkBackground = Color(0xFF0F0F0F)
+val Black200 = Color(0xFF1F1F1F)
+val Black700 = Color(0xFF171717)
+val Blue200 = Color(0xFF33A3FF)
+val Blue500 = Color(0xFF008AFF)
+val Blue700 = Color(0xFF0070CC)
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
